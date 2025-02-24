917ac9362f89adc4e50443c266f02a4d0b2e81b8
Patty Ouwehand <patty@g2g-online.nl>
2025-02-24 20:45:54 +0100

    clean up

--- app/todo/Todos.js
+++ app/todo/Todos.js
@@ -5,20 +5,20 @@ import InputField from '../components/molecules/InputField'
 import { FaRegEdit } from 'react-icons/fa'
 import { MdDeleteForever } from 'react-icons/md'
 
-const API_URL = 'https://xlz20tg1fd.execute-api.eu-north-1.amazonaws.com/prod/todos'
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 const Todos = () => {
   const [userInput, setUserInput] = useState('')
   const [list, setList] = useState([])
 
-  // Fetch existing todos when the component mounts
   useEffect(() => {
     const fetchTodos = async () => {
       try {
         const response = await fetch(API_URL)
         if (!response.ok) throw new Error("Failed to fetch todos")
         const data = await response.json()
-        setList(data) // Assuming API returns an array of todos
+        setList(data)
+
       } catch (error) {
         console.error("Error fetching todos:", error)
       }
@@ -30,7 +30,6 @@ const Todos = () => {
 
   const addItem = async () => {
     if (!userInput.trim()) return
-
     const newTodo = { value: userInput }
 
     try {
@@ -60,7 +59,6 @@ const Todos = () => {
       })
 
       if (!response.ok) throw new Error("Failed to delete todo")
-
       setList(list.filter(item => item._id !== _id))
 
     } catch (error) {
@@ -71,7 +69,6 @@ const Todos = () => {
   const editItem = async (index) => {
     const newValue = prompt('Edit your todo', list[index].value)
     if (!newValue || newValue.trim() === "") return
-
     const updatedTodo = { ...list[index], value: newValue }
 
     try {
@@ -82,7 +79,6 @@ const Todos = () => {
       })
 
       if (!response.ok) throw new Error("Failed to update todo")
-
       const updatedTodos = [...list]
       updatedTodos[index] = updatedTodo
       setList(updatedTodos)
@@ -109,7 +105,6 @@ const Todos = () => {
           Add new task
         </button>
       </div>
-
       {
         list.length > 0 ? (
           list.map((item, index) => (
@@ -118,7 +113,9 @@ const Todos = () => {
               key={`list-item-${index}`}
             >
               <div className="flex justify-between items-center w-full">
-                <div className="block basis-5/6 break-words">{item.value}</div>
+                <div className="block basis-5/6 break-words">
+                  {item.value}
+                </div>
                 <div className="flex basis-1/6 gap-1 md:gap-3">
                   <button
                     className="bg-transparent focus:bg-transparent active:bg-transparent hover:bg-transparent"
